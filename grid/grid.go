cefbe008bc179c3321a8c265fdd36ce59e01847f
guiyuanju <guiyuanjune@outlook.com>
2025-07-18 15:20:18 +0800

    add grid strings & bytes

--- grid/grid.go
+++ grid/grid.go
@@ -15,7 +15,7 @@ func Print(g [][]int) {
 	}
 }
 
-func New(s string) [][]int {
+func Ints(s string) [][]int {
 	s = s[1 : len(s)-1]
 	parts := strings.Split(s, "],[")
 	var res [][]int
@@ -33,6 +33,46 @@ func New(s string) [][]int {
 				panic(err)
 			}
 			cur = append(cur, int(n))
+		}
+		res = append(res, cur)
+	}
+	return res
+}
+
+func Strings(s string) [][]string {
+	s = s[1 : len(s)-1]
+	parts := strings.Split(s, "],[")
+	var res [][]string
+	for _, p := range parts {
+		p = strings.Trim(p, "[]")
+		if len(p) == 0 {
+			res = append(res, []string{})
+			continue
+		}
+		ints := strings.Split(p, ",")
+		var cur []string
+		for _, i := range ints {
+			cur = append(cur, strings.Trim(i, "\""))
+		}
+		res = append(res, cur)
+	}
+	return res
+}
+
+func Bytes(s string) [][]byte {
+	s = s[1 : len(s)-1]
+	parts := strings.Split(s, "],[")
+	var res [][]byte
+	for _, p := range parts {
+		p = strings.Trim(p, "[]")
+		if len(p) == 0 {
+			res = append(res, []byte{})
+			continue
+		}
+		ints := strings.Split(p, ",")
+		var cur []byte
+		for _, i := range ints {
+			cur = append(cur, strings.Trim(i, "\"")[0])
 		}
 		res = append(res, cur)
 	}
